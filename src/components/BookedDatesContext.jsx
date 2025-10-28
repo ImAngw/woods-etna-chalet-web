a54d6846d256ac03f8bbb916ac669f1e12372545
angelo <angelocapo94@gmail.com>
2025-10-28 19:06:45 +0100

    Updates

--- src/components/BookedDatesContext.jsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-export const BookedDatesContext = createContext({
-    bookedDates: new Set(),
-    setBookedDates: () => {}
-});
--- src/components/mySupabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-
-const supabaseURL = "https://thncfttcsauysykhktnn.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRobmNmdHRjc2F1eXN5a2hrdG5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExMzk0OTksImV4cCI6MjA3NjcxNTQ5OX0.n4i6kGUBghUD787zN1i9vtawLWd9D8Il8JthD8NCrCo"
-
-export const supabase = createClient(supabaseURL, supabaseKey)
